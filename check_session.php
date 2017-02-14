a118d75ca557cf0b357f5d3c8af45f351aa14e95
Charmaine Calsas <cpcalsas011@gmail.com>
2017-02-14 22:10:23 +0800

    changed url to ticketing

--- check_session.php
+++ check_session.php
@@ -2,6 +2,6 @@
 	session_start();
 	
 	if($_SESSION['username']==""){
-		echo "<script>window.location='http://localhost/Thesis_ticketing/login.php'</script>";
+		echo "<script>window.location='http://localhost/ticketing/login.php'</script>";
 	}
 ?>
