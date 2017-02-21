e95a9c92ca57b311efe5bd9f5304cf933285cdea
Charmaine Calsas <cpcalsas011@gmail.com>
2017-02-21 13:43:13 +0800

    get session - user's name on topbar

--- includes/topbar.php
+++ includes/topbar.php
@@ -36,8 +36,15 @@ require_once 'connection.php';
             <ul class="nav navbar-nav navbar-right user-nav">
                 <li class="dropdown profile_menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img alt="Avatar" src="images/avatar2.jpg" />
-                    
-                    <span>Jason Gicha</span> <b class="caret"></b></a>
+
+                        <?php
+                        $loggeduser = $_SESSION['admin'];
+                        $sql = "SELECT * FROM users WHERE username = ?";
+                        $res = $db->prepare($sql);
+                        $res->execute(array($loggeduser));
+                        $row = $res->fetch(PDO::FETCH_NUM);
+                        ?>
+                        <span><?php echo $row[3] . " " . $row[5]; ?></span> <b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li><a href="#">My Account</a></li>
                         <li><a href="#">Profile</a></li>
