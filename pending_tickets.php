d56b9e67fe784557f80d3c996a9bb2ecf63e33ac
ZWASZE\Gecha <adampacks14@gmail.com>
2017-07-04 16:54:38 +0800

    logo

--- pending_tickets.php
+++ pending_tickets.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
-        <!--<link rel="shortcut icon" href="images/favicon.png">-->
+        <link rel="shortcut icon" href="images/fb-art1.png">
 
         <title>Fortis Ticketing System</title>
         <!--<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,400italic,700,800' rel='stylesheet' type='text/css'>
