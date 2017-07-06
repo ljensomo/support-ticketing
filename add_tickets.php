6f3cdc9ade7fdf926af792f8c64e0c7a8cedf986
Kurthjohn\Kurth John P.Quimora <kurthjohnquimora@gmail.com>
2017-07-06 15:38:20 +0800

    add tickets fixed

--- add_tickets.php
+++ add_tickets.php
@@ -58,20 +58,12 @@
                             
                                 <ul style="list-style: none;" class="nav nav-pills nav-stacked">
                                 <h4 style="background-color: #C94848; color:white; padding: 8px;">Menu</h4>
-<<<<<<< HEAD
+
                                     <li  style=" padding: 6px"><a href="homebanner.php" style="color:black; font-size: 140%">Home</a></li>
                                     <li  style=" padding: 6px"><a href="add_tickets.php" style="color:black; font-size: 140%">Tickets</a></li>
                                     <li  style=" padding: 6px"><a href="#" style="color:black; font-size: 140%">Projects</a></li>
                                     <li  class="active" style=" padding: 6px"><a href="create.php" style="color:black; font-size: 140%">Create Ticket</a></li>
                                     <li  style=" padding: 6px"><a href="#" style="color:black; font-size: 140%">Users</a></li>
-=======
-                                    <li   style=" padding: 6px"><a href="homebanner.php" style="color:black; font-size: 140%">Home</a></li>
-                                    <li   style=" padding: 6px"><a href="add_tickets.php" style="color:black; font-size: 140%">Tickets</a></li>
-                                    <li  style=" padding: 6px"><a href="project.php" style="color:black; font-size: 140%">Projects</a></li>
-                                    <li  class="active" style=" padding: 6px"><a href="create.php" style="color:black; font-size: 140%">Create Ticket</a></li>
-                                    <li style=" padding: 6px"><a href="#" style="color:black; font-size: 140%">Users</a></li>
->>>>>>> 66d177042fa01c1979d9eb4909683dd078265d98
-
                                 </ul>
                             </div>
                             </div>
