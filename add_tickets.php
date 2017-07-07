ef59458a3f34a302b4ce0cfb6bafcb36c8e0da95
LAPTOP-JJ9SRRND\Andrian <andanarandrian@gmail.com>
2017-07-07 13:03:20 +0800

    update add_tickets

--- add_tickets.php
+++ add_tickets.php
@@ -59,8 +59,8 @@
                                 <ul style="list-style: none;" class="nav nav-pills nav-stacked">
 
                                     <li  style=" padding: 6px"><a href="homebanner.php" style="color:black; font-size: 140%">Home</a></li>
-                                    <li  style=" padding: 6px"><a href="add_tickets.php" style="color:black; font-size: 140%">Tickets</a></li>
-                                    <li  style=" padding: 6px"><a href="projects.php" style="color:black; font-size: 140%">Projects</a></li>
+                                    <li  style=" padding: 6px"><a href="homebanner_tickets.php" style="color:black; font-size: 140%">Tickets</a></li>
+                                    <li  style=" padding: 6px"><a href="project.php" style="color:black; font-size: 140%">Projects</a></li>
                                     <li  class="active" style=" padding: 6px"><a href="#" style="color:black; font-size: 140%">Create Ticket</a></li>
                                     <li  style=" padding: 6px"><a href="#" style="color:black; font-size: 140%">Users</a></li>
                                 </ul>
