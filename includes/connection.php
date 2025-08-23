b29c00794fbe9e8d967449fc3291c4c1d03e804b
LJ Ensomo <ensomolj@gmail.com>
2025-08-23 22:54:07 +0800

    fixed login

--- includes/connection.php
+++ includes/connection.php
@@ -1,26 +1,24 @@
 <?php
+    class Database extends PDO {
 
-class Database extends pdo {
+        private $dbtype;
+        private $host;
+        private $user;
+        private $pass;
+        private $database;
 
-    private $dbtype;
-    private $host;
-    private $user;
-    private $pass;
-    private $database;
-
-    public function __construct() {
-        $this->dbtype = 'mysql';
-        $this->host = 'localhost';
-        $this->user = 'root';
-        $this->pass = '';
-        $this->database = 'ticketing';
-        $dns = $this->dbtype . ':dbname=' . $this->database . ";host=" . $this->host;
-        parent::__construct($dns, $this->user, $this->pass);
+        public function __construct() {
+            $this->dbtype = 'mysql';
+            $this->host = 'localhost';
+            $this->user = 'root';
+            $this->pass = '';
+            $this->database = 'ticketing';
+            $dns = $this->dbtype . ':dbname=' . $this->database . ";host=" . $this->host;
+            parent::__construct($dns, $this->user, $this->pass);
+        }
     }
 
-}
-
-$database = new Database();
-$db = & $database;
+    $database = new Database();
+    $db = & $database;
 ?>
 
--- includes/login.php
+++ includes/login.php
@@ -1,80 +1,67 @@
-
 <?php
+    session_start();
+    include 'connection.php';
 
-session_start();
-include 'connection.php';
-include 'functions.php';
-
-$user = $_POST['user'];
-$pw = $_POST['pw'];
-
-$sql = "SELECT 
-	a.user_id,	
-	a.fname,
-	a.mname,
-	a.lname,
-	a.company_id,
-	a.cnum,
-	a.email,
-	a.is_active,
-	a.uname,
-	a.pass,
-	d.user_desc,
-	d.userlevel_id
-	
-FROM users AS a JOIN users_roles AS c 
-ON a.user_id=c.user_id
-JOIN roles AS d 
-ON c.user_role=d.userlevel_id WHERE uname = BINARY ?";
-
-$res = $db->prepare($sql);
-$res->execute(array($user));
-$row = $res->fetch(PDO::FETCH_NUM);
-
-$required = array($user, $pw);
-$error = false;
-foreach ($required as $fields) {
-    if (empty($fields))
- {
-        $error = true;
+    if(!isset($_POST['username']) || !isset($_POST['password'])) {
+        echo json_encode(array('success' => false, 'message' => 'Invalid login access.'));
     }
-}
 
-if ($error) {
+    $user = $_POST['username'];
+    $pw = $_POST['password'];
 
-    openWindow($goto = "../login.html");
-} else if ($row[0] == "") {
-    echo "none";
-    //msgAlert($alert = "The username and password you entered did not match our records. Please double-check and try again.");
-    //openWindow($goto = "../login.html");
-} else if ($row[7] == 0) {
-	echo "not active";
-    //msgAlert($alert = "The user you entered is not yet activated. Please contact your administrator to activate your account.");
-    //openWindow($goto = "../login.html");
-} else if (password_verify($pw, $row[9])) {
-    $_SESSION['admin'] = $row[8];
-    if($row[11]==4 || $row[11]==5){
-         //openWindow($goto = "../homebanner.php");
-		 echo "client";
-    } else{
-	     echo "admin";
-    //openWindow($goto = "../index.php");
-	}
+    // $sql = "SELECT 
+    // 	a.user_id,	
+    // 	a.fname,
+    // 	a.mname,
+    // 	a.lname,
+    // 	a.company_id,
+    // 	a.cnum,
+    // 	a.email,
+    // 	a.is_active,
+    // 	a.uname,
+    // 	a.pass,
+    // 	d.user_desc,
+    // 	d.userlevel_id
+        
+    // FROM users AS a JOIN users_roles AS c 
+    // ON a.user_id=c.user_id
+    // JOIN roles AS d 
+    // ON c.user_role=d.userlevel_id WHERE uname = BINARY ?";
 
-    $query = "UPDATE users SET line_status = ? WHERE uname = ?";
-    $stmt = $db->prepare($query);
-    $stmt->execute(array(1,$user));
-    /*if ($row['userlevel_id'] == '1') {
-        $_SESSION['admin'] = $row['username'];
-        msgAlert($alert = "Login Successful");
-        openWindow($goto = "../index.php");
-    } else {
-        msgAlert($alert = "Access Denied");
-        openWindow($goto = "../login.html");
-    }*/
-} else if (!password_verify($pw, $row[9])) {
-    echo "incorrect";
-    //msgAlert($alert = "The password you entered is incorrect. Please try again");
-    //openWindow($goto = "../login.html");
-}
+    $sql = 'SELECT * FROM users WHERE username = BINARY ?';
+
+    $res = $db->prepare($sql);
+    $res->execute(array($user));
+    $row = $res->fetch(PDO::FETCH_NUM);
+
+    if ($res->rowCount() == 0) {
+        echo json_encode(array('success' => false, 'message' => 'Invalid login credentials.'));
+    } else if ($row[7] == 0) {
+        echo json_encode(array('success' => false, 'message' => 'User account is not activated.'));
+    } else if (!password_verify($pw, $row[9])) {
+        $_SESSION['admin'] = $row[8];
+        // if($row[11]==4 || $row[11]==5){
+        //      //openWindow($goto = "../homebanner.php");
+        // 	 echo "client";
+        // } else{
+        //      echo "admin";
+        // //openWindow($goto = "../index.php");
+        // }
+
+        $query = "UPDATE users SET is_online = ? WHERE id = ?";
+        $stmt = $db->prepare($query);
+        $stmt->execute(array(1,$row[0]));
+        /*if ($row['userlevel_id'] == '1') {
+            $_SESSION['admin'] = $row['username'];
+            msgAlert($alert = "Login Successful");
+            openWindow($goto = "../index.php");
+        } else {
+            msgAlert($alert = "Access Denied");
+            openWindow($goto = "../login.html");
+        }*/
+
+        echo json_encode(array('success' => true, 'message' => 'Login successful.'));
+    } else if (!password_verify($pw, $row[9])) {
+        echo json_encode(array('success' => false, 'message' => 'The password you entered is incorrect. Please try again.'));
+    }
 ?>
